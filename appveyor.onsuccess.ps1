3dc2b418da4cb70099b6e4a63470329673f5a341
Andreas Grünwald <andreasgruenwald@live.de>
2017-07-15 18:01:23 +0200

    Fix appveyor.onsuccess.ps1

--- appveyor.onsuccess.ps1
+++ appveyor.onsuccess.ps1
@@ -2,4 +2,4 @@
 Invoke-Expression "git config --global credential.helper store"
 Add-Content "$env:USERPROFILE\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n"  
 Invoke-Expression "git tag v$($env:APPVEYOR_BUILD_VERSION) $($env:APPVEYOR_REPO_COMMIT)"
-Invoke-Expresion "git push origin v$($env:APPVEYOR_BUILD_VERSION)"
+Invoke-Expression "git push origin v$($env:APPVEYOR_BUILD_VERSION)"
